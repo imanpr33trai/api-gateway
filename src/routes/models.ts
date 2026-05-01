c20f310705e813ae00553592d18851251d4680d3
Manpreet Singh <manpreet.singh01356@gmail.com>
2026-05-01 11:11:46 +0530

    it is working well, and the changes will in the new branch

--- src/routes/models.ts
+++ src/routes/models.ts
@@ -4,16 +4,4 @@ import { modelController } from "../controller/model.controller";
 
 export const modelsRoute = new Hono();
 
-modelsRoute.get("/", modelController).get("/tags", async (c) => {
-     try {
-          const data = await Bun.file("models.json").json();
-          return c.json(data);
-     } catch (error) {
-          const message =
-               error instanceof Error ? error.message : String(error);
-          return c.json(
-               { error: `Failed to read models.json, ${message}` },
-               500,
-          );
-     }
-});
+modelsRoute.get("/", modelController);
